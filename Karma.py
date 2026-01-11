f8b9779a6606b2df26a022711d9339a988c72a7c
Al <rosser.williamwhite@gmail.com>
2026-01-11 15:15:28 +1000

    setup buggy game

--- Karma.py
+++ Karma.py
@@ -6,75 +6,73 @@ import random
 # 1 of 2 and face cards
 
 
-
 def go_bot1(deck, discard, player):
-    ''' Dummy bot, just plays the most of the lowest ranking card '''
+    """Dummy bot, just plays the most of the lowest ranking card"""
     # always
     fail = 0
-    inplay = player['hand'] if player['hand'] else player['known'] if player['known'] else player['unknown']
-    
+    inplay = player["hand"] if player["hand"] else player["known"] if player["known"] else player["unknown"]
+
     if not inplay:
         return 1
-
 
     if discard == []:
         # play the most of the lowest card
         lowest = inplay[0]
         indices = [i for i, card in enumerate(inplay) if card == lowest]
-        discard += inplay[indices[0]:indices[-1]+1]
-        del inplay[indices[0]:indices[-1]+1]
+        discard += inplay[indices[0] : indices[-1] + 1]
+        del inplay[indices[0] : indices[-1] + 1]
     else:
         # play the most of the next highest card
         greater = [i for i, card in enumerate(inplay) if card >= discard[-1]]
         # try hand
-        if player['hand']: 
-            if greater: 
-                lowest = player['hand'][greater[0]]
-                indices = [i for i, card in enumerate(player['hand']) if card == lowest]
-                discard += player['hand'][indices[0]:indices[-1]+1]
-                del player['hand'][indices[0]:indices[-1]+1]
+        if player["hand"]:
+            if greater:
+                lowest = player["hand"][greater[0]]
+                indices = [i for i, card in enumerate(player["hand"]) if card == lowest]
+                discard += player["hand"][indices[0] : indices[-1] + 1]
+                del player["hand"][indices[0] : indices[-1] + 1]
             else:
                 fail = 1
         # try known
-        elif player['known']:
+        elif player["known"]:
             if greater:
-                lowest = player['known'][greater[0]]
-                index = player['known'].index(lowest)
-                discard.append(player['known'][index])
-                del player['known'][index]
+                lowest = player["known"][greater[0]]
+                index = player["known"].index(lowest)
+                discard.append(player["known"][index])
+                del player["known"][index]
             else:
                 fail = 1
-        elif player['unknown']:
-            discard.append(player['unknown'].pop(0))
+        elif player["unknown"]:
+            discard.append(player["unknown"].pop(0))
             # check for fail
             if discard[-2] > discard[-1]:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
         else:
             return 1
 
         # check for fail
         if fail:
-            # try play a 2 
-            if card_index[2] in player['hand']:
+            # try play a 2
+            if card_index[2] in player["hand"]:
                 discard.append(card_index[2])
-                player['hand'].remove(card_index[2])
+                player["hand"].remove(card_index[2])
             # pick up
             else:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
 
     # if a quad discard and go again
     if len(discard) > 3:
         if discard[-4:] == [discard[-1]] * 4:
             discard.clear()
             # fill cards otherwise
-            while len(player['hand']) < 3 and deck != []:
-                player['hand'].append(deck.pop(0))
+            while len(player["hand"]) < 3 and deck != []:
+                player["hand"].append(deck.pop(0))
 
-            player['hand'].sort()
+            player["hand"].sort()
             win = go_bot1(deck, discard, player)
             if win:
                 return 1
@@ -83,26 +81,27 @@ def go_bot1(deck, discard, player):
     if discard[-1] == card_index[10]:
         discard.clear()
         # fill cards otherwise
-        while len(player['hand']) < 3 and deck != []:
-            player['hand'].append(deck.pop(0))
+        while len(player["hand"]) < 3 and deck != []:
+            player["hand"].append(deck.pop(0))
 
-        player['hand'].sort()
+        player["hand"].sort()
         win = go_bot1(deck, discard, player)
         if win:
             return 1
-    
-    # fill cards otherwise
-    while len(player['hand']) < 3 and deck != []:
-        player['hand'].append(deck.pop(0))
 
-    player['hand'].sort()
+    # fill cards otherwise
+    while len(player["hand"]) < 3 and deck != []:
+        player["hand"].append(deck.pop(0))
+
+    player["hand"].sort()
+
 
 def go_bot2(deck, discard, player):
-    ''' Ensure that twos are played less often '''
+    """Ensure that twos are played less often"""
     # always
     fail = 0
-    inplay = player['hand'] if player['hand'] else player['known'] if player['known'] else player['unknown']
-    
+    inplay = player["hand"] if player["hand"] else player["known"] if player["known"] else player["unknown"]
+
     if not inplay:
         return 1
 
@@ -117,8 +116,8 @@ def go_bot2(deck, discard, player):
             lowest = nonzeros[0]
 
         indices = [i for i, card in enumerate(inplay) if card == lowest]
-        discard += inplay[indices[0]:indices[-1]+1]
-        del inplay[indices[0]:indices[-1]+1]
+        discard += inplay[indices[0] : indices[-1] + 1]
+        del inplay[indices[0] : indices[-1] + 1]
     else:
         # play the most of the next highest card
         if discard[-1] != 0:
@@ -127,54 +126,54 @@ def go_bot2(deck, discard, player):
             # ensure zero is not unecessarily played
             greater = [i for i, card in enumerate(inplay) if card > discard[-1]]
         # try hand
-        if player['hand']: 
-            if greater: 
-                lowest = player['hand'][greater[0]]
-                indices = [i for i, card in enumerate(player['hand']) if card == lowest]
-                discard += player['hand'][indices[0]:indices[-1]+1]
-                del player['hand'][indices[0]:indices[-1]+1]
+        if player["hand"]:
+            if greater:
+                lowest = player["hand"][greater[0]]
+                indices = [i for i, card in enumerate(player["hand"]) if card == lowest]
+                discard += player["hand"][indices[0] : indices[-1] + 1]
+                del player["hand"][indices[0] : indices[-1] + 1]
             else:
                 fail = 1
         # try known
-        elif player['known']:
+        elif player["known"]:
             if greater:
-                lowest = player['known'][greater[0]]
-                index = player['known'].index(lowest)
-                discard.append(player['known'][index])
-                del player['known'][index]
+                lowest = player["known"][greater[0]]
+                index = player["known"].index(lowest)
+                discard.append(player["known"][index])
+                del player["known"][index]
             else:
                 fail = 1
-        elif player['unknown']:
-            discard.append(player['unknown'].pop(0))
+        elif player["unknown"]:
+            discard.append(player["unknown"].pop(0))
             # check for fail
             if discard[-2] > discard[-1]:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
         else:
             return 1
 
         # check for fail
         if fail:
-            # try play a 2 
-            if card_index[2] in player['hand']:
+            # try play a 2
+            if card_index[2] in player["hand"]:
                 discard.append(card_index[2])
-                player['hand'].remove(card_index[2])
+                player["hand"].remove(card_index[2])
             # pick up
             else:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
 
     # if a quad discard and go again
     if len(discard) > 3:
         if discard[-4:] == [discard[-1]] * 4:
             discard.clear()
             # fill cards otherwise
-            while len(player['hand']) < 3 and deck != []:
-                player['hand'].append(deck.pop(0))
+            while len(player["hand"]) < 3 and deck != []:
+                player["hand"].append(deck.pop(0))
 
-            player['hand'].sort()
+            player["hand"].sort()
             win = go_bot2(deck, discard, player)
             if win:
                 return 1
@@ -183,32 +182,33 @@ def go_bot2(deck, discard, player):
     if discard[-1] == card_index[10]:
         discard.clear()
         # fill cards otherwise
-        while len(player['hand']) < 3 and deck != []:
-            player['hand'].append(deck.pop(0))
+        while len(player["hand"]) < 3 and deck != []:
+            player["hand"].append(deck.pop(0))
 
-        player['hand'].sort()
+        player["hand"].sort()
         win = go_bot2(deck, discard, player)
         if win:
             return 1
-    
-    # fill cards otherwise
-    while len(player['hand']) < 3 and deck != []:
-        player['hand'].append(deck.pop(0))
 
-    player['hand'].sort()
+    # fill cards otherwise
+    while len(player["hand"]) < 3 and deck != []:
+        player["hand"].append(deck.pop(0))
+
+    player["hand"].sort()
+
 
 def go_bot3(deck, discard, player):
-    ''' 
-    From a mapping find the closest card to play and play it 
-    mapping from [2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A] 
+    """
+    From a mapping find the closest card to play and play it
+    mapping from [2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A]
               to [3, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A] then [2, 10]
-    
-    '''
+
+    """
 
     # always
     fail = 0
-    inplay = player['hand'] if player['hand'] else player['known'] if player['known'] else player['unknown']
-    
+    inplay = player["hand"] if player["hand"] else player["known"] if player["known"] else player["unknown"]
+
     if not inplay:
         return 1
 
@@ -223,8 +223,8 @@ def go_bot3(deck, discard, player):
             lowest = nonzeros[0]
 
         indices = [i for i, card in enumerate(inplay) if card == lowest]
-        discard += inplay[indices[0]:indices[-1]+1]
-        del inplay[indices[0]:indices[-1]+1]
+        discard += inplay[indices[0] : indices[-1] + 1]
+        del inplay[indices[0] : indices[-1] + 1]
     else:
         # play the most of the next highest card
         if discard[-1] != 0:
@@ -233,54 +233,54 @@ def go_bot3(deck, discard, player):
             # ensure zero is not unecessarily played
             greater = [i for i, card in enumerate(inplay) if card > discard[-1]]
         # try hand
-        if player['hand']: 
-            if greater: 
-                lowest = player['hand'][greater[0]]
-                indices = [i for i, card in enumerate(player['hand']) if card == lowest]
-                discard += player['hand'][indices[0]:indices[-1]+1]
-                del player['hand'][indices[0]:indices[-1]+1]
+        if player["hand"]:
+            if greater:
+                lowest = player["hand"][greater[0]]
+                indices = [i for i, card in enumerate(player["hand"]) if card == lowest]
+                discard += player["hand"][indices[0] : indices[-1] + 1]
+                del player["hand"][indices[0] : indices[-1] + 1]
             else:
                 fail = 1
         # try known
-        elif player['known']:
+        elif player["known"]:
             if greater:
-                lowest = player['known'][greater[0]]
-                index = player['known'].index(lowest)
-                discard.append(player['known'][index])
-                del player['known'][index]
+                lowest = player["known"][greater[0]]
+                index = player["known"].index(lowest)
+                discard.append(player["known"][index])
+                del player["known"][index]
             else:
                 fail = 1
-        elif player['unknown']:
-            discard.append(player['unknown'].pop(0))
+        elif player["unknown"]:
+            discard.append(player["unknown"].pop(0))
             # check for fail
             if discard[-2] > discard[-1]:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
         else:
             return 1
 
         # check for fail
         if fail:
-            # try play a 2 
-            if card_index[2] in player['hand']:
+            # try play a 2
+            if card_index[2] in player["hand"]:
                 discard.append(card_index[2])
-                player['hand'].remove(card_index[2])
+                player["hand"].remove(card_index[2])
             # pick up
             else:
-                player['hand'] += discard
+                player["hand"] += discard
                 discard.clear()
-                player['hand'].sort()
+                player["hand"].sort()
 
     # if a quad discard and go again
     if len(discard) > 3:
         if discard[-4:] == [discard[-1]] * 4:
             discard.clear()
             # fill cards otherwise
-            while len(player['hand']) < 3 and deck != []:
-                player['hand'].append(deck.pop(0))
+            while len(player["hand"]) < 3 and deck != []:
+                player["hand"].append(deck.pop(0))
 
-            player['hand'].sort()
+            player["hand"].sort()
             win = go_bot2(deck, discard, player)
             if win:
                 return 1
@@ -289,22 +289,23 @@ def go_bot3(deck, discard, player):
     if discard[-1] == card_index[10]:
         discard.clear()
         # fill cards otherwise
-        while len(player['hand']) < 3 and deck != []:
-            player['hand'].append(deck.pop(0))
+        while len(player["hand"]) < 3 and deck != []:
+            player["hand"].append(deck.pop(0))
 
-        player['hand'].sort()
+        player["hand"].sort()
         win = go_bot2(deck, discard, player)
         if win:
             return 1
-    
-    # fill cards otherwise
-    while len(player['hand']) < 3 and deck != []:
-        player['hand'].append(deck.pop(0))
 
-    player['hand'].sort()
+    # fill cards otherwise
+    while len(player["hand"]) < 3 and deck != []:
+        player["hand"].append(deck.pop(0))
+
+    player["hand"].sort()
+
 
 def deal():
-    deck = list(range(0,13)) * 4 # repeat 0-12 4 times
+    deck = list(range(0, 13)) * 4  # repeat 0-12 4 times
     random.shuffle(deck)
     old_deck = deck
 
@@ -314,20 +315,22 @@ def deal():
     # initialise hands
     players = [p1, p2]
     for i, player in enumerate(players):
-        known = player[0:6]; known.sort()
+        known = player[0:6]
+        known.sort()
         unknown = player[6:9]
-        player = {'hand': known[0:3], 'known': known[3:6], 'unknown': unknown}
+        player = {"hand": known[0:3], "known": known[3:6], "unknown": unknown}
         players[i] = player
     return players, deck
 
+
 def botbot_game():
-    card_map = [2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K', 'A', 10]
+    card_map = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A", 10]
     global card_index
-    card_index = { val: i for i, val in enumerate(card_map)}
+    card_index = {val: i for i, val in enumerate(card_map)}
     # someone goes first
     wins = []
     for k in range(100):
-        print('New Game:')
+        print("New Game:")
         players, deck = deal()
         discard = []
 
@@ -337,17 +340,17 @@ def botbot_game():
         while turn < 1000 and not win:
             player = players[p]
             # take turns
-            print(f'Player {p}') 
-            print(f'Hand: {player['hand']} Known: {player['known']}')
+            print(f"Player {p}")
+            print(f"Hand: {player['hand']} Known: {player['known']}")
             win = go_bot1(deck, discard, player)
             if win:
                 # print(f'player {p} won')
                 wins.append(p)
-            print('Discard:')
+            print("Discard:")
             print(discard)
             turn += 1
             p = 0 if p else 1
-    print(f'Player 0 won {wins.count(0)/len(wins)*100:.2f}% of the time')
+    print(f"Player 0 won {wins.count(0)/len(wins)*100:.2f}% of the time")
+
 
 botbot_game()
-
--- KarmaV2.py
+++ KarmaV2.py
@@ -5,6 +5,7 @@ import functools
 # long numpy array to store previous cards, phase, hand
 # choose which cards to play out of hand
 
+
 class Player:
     # player has a hand, known and unknown
     def __init__(self):
@@ -14,65 +15,96 @@ class Player:
     def inplay(self):
         return self.hand if self.hand else self.known if self.known else self.unknown
 
+    def getbi(self, pile):
+        print(f'Pile: {pile}')
+        print(f'In play: {self.inplay()}')
+        bi_list = input("Input bi separated by spaces: ").split()
+        bi_floated = [float(num) for num in bi_list]
+        return tuple(bi_floated)
+
 class Karma:
-    def __init__(self, n_players: int):
-        self.pack = random.shuffle(list(range(0,13)) * 4) # repeat 0-12 4 times
-        self.draw = self.pack[n_players*9:]
+    def __init__(self, n_players: int = 2):
+        self.pack = list(range(0, 13)) * 4
+        random.shuffle(self.pack)  # repeat 0-12 4 times
+        self.draw = self.pack[n_players * 9 :]
         self.deal(n_players)
-        self.discard = []
+        self.pile = self.oldpile = []
+        self.whosturn = 0
+        self.win = False
 
     def deal(self, n_players):
+        # TODO debug: players only have two cards
         self.players = []
         for i in range(n_players):
             player = Player()
-            hand_start = i*9; known_start = i*9+3; unknown_start = i*9+6
-            player.hand = self.pack[hand_start:hand_start+2]
-            player.known = self.pack[known_start:known_start+2]
-            player.unknown = self.pack[unknown_start:unknown_start+2]
+            hand_start = i * 9; known_start = i * 9 + 3; unknown_start = i * 9 + 6
+            player.hand = self.pack[hand_start : hand_start + 2]
+            player.known = self.pack[known_start : known_start + 2]
+            player.unknown = self.pack[unknown_start : unknown_start + 2]
             self.players.append(player)
 
     # Turn methods
+    def getplayer(self) -> Player:
+        return self.players[self.whosturn]
+
     def play(self, player: Player, bi: tuple):
+        # TODO debug behaviour is weird
         inplay = player.inplay()
-        ind = [round(bi[0]*len(inplay)), round(bi[1]*len(inplay))]
-        # TODO remove players cards
-        self.discard = self.discard + inplay[ind[0]:ind[1]] if ind[0] != ind[1] else self.discard
+        ind = [round(bi[0] * len(inplay)), round(bi[1] * len(inplay))]
+        del inplay[ind[0] : ind[1]]
+        self.added = inplay[ind[0] : ind[1]]
+        print(f'Added: {self.added}')
+        self.oldpile = self.pile
+        self.pile += self.added
+
+    def rulebook(self, player: Player):
+        if self.added == []:
+            return 'fail'
+        elif len(set(self.added)) > 1:
+            return 'fail'
+
+        if self.oldpile != []:
+            if self.oldpile[-1] > self.added[0]:
+                player.hand.append(self.pile)
+                self.pile = []
+        # TODO other rules 
+        return 'success'  
     
-    def referee(self, player: Player):
-        if self.discard[-1] > self.discard[-2]:
-            player.hand.append(self.discard)
-            self.discard = []
-        elif player.hand == [] and player.known == [] and player.unknown == []:
+    def nextplayer(self):
+        self.whosturn = (self.whosturn + 1) % len(self.players)
+
+    def checkwin(self, player: Player):
+        if player.hand == [] and player.known == [] and player.unknown == []:
             player.win = True
-        # TODO other rules
+            print(f"Player {self.whosturn} won!")
 
-    def governor(self, player: Player, outcome: str):
-        if outcome == 'fail':
-            self.fail()
-        elif outcome == 'bomb':
-            self.bomb()
-            # TODO its the same players turn again
-        # TODO returns next player, same player or win
+    def referee(self, player: Player, outcome: str) -> int:
+        print(outcome)
+        if outcome == "fail":
+            player.hand += self.pile
+            self.pile = []
+        elif outcome == "bomb":
+            self.pile = []
+            self.turn()
+        # TODO player refill
+        self.checkwin(player)
+        
+    def turn(self):
+        player: Player = self.getplayer()
+        bi = player.getbi(self.pile)
+        self.play(player, bi)
+        outcome = self.rulebook(player)
+        self.referee(player, outcome)
+        self.nextplayer()   
 
-    def turn(self, player: Player, bi: tuple):
-        self.play(self, player, bi)
-        outcome = self.referee(self, player)
-        return self.governor(self, player, outcome) 
-    
     # Game control
-    def logic(self):
-        # choose player to go
-        # get bi
-        # turn
-        # repeat
-        return self.turn()
+    def run(self):
+        while not self.win:
+            self.turn()
 
-# construct referee  
+# construct referee
 # should just work by inputing start, stop
 
 game = Karma()
-# choose player to go
-# get bi
-# turn
-# repeat
-
+# to check valid play, before checking any other rules check if pile[old_pile_len:] is not all the same
+game.run()
