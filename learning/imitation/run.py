ebb7c0d3826aba9c6f164dd8b2c7b4b83d86bbc5
rosserwilliamwhite <rosser.williamwhite@gmail.com>
2026-02-06 16:08:56 +1000

    refactor for use as module

--- learning/imitation/run.py
+++ learning/imitation/run.py
@@ -3,11 +3,11 @@ import torch
 import numpy as np
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='learning/imitation/run.log', level=logging.INFO, filemode='w')
 
-net = torch.load('learning/imitation/imitation.pt', weights_only=False)
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='learning/imitation/run.log', level=logging.INFO, filemode='w')
+    net = torch.load('learning/imitation/imitation.pt', weights_only=False)
     info = ('bot',net)
     for i in range(100):
         game = Karma(info)
